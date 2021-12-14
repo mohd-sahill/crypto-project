eca37c13025d90a3da8c4f6d08749ef5a721a11c
mohd-sahill <mohammedsahil692@gmail.com>
2021-12-14 21:22:16 +0530

    new project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import doge from "./images/dogecoin.png";
+import { FiRefreshCcw } from "react-icons/fi";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=true"
+    )
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        throw new Error("404")
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <div className="header">
+          <img src={doge} alt="" />
+          <h1>Welcome to DogeZone</h1>
+          <div className="input-section">
+            <input type="text" placeholder="SEARCH FOR A CRYPTOCURRENCY" />
+            <button className="btn">
+              {" "}
+              <FiRefreshCcw color="#ffd662ff" size="25px" />
+            </button>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
 
---
+++ src/noResponse.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function noResponse() {
+    return (
+        <div>
+            <h1>no response 404</h1>
+        </div>
+    )
+}
